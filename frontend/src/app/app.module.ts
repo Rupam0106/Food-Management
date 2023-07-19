b44c3811ac0737e29c35dc02fa916aec4878c17b
Rupam0106 <rupam.gupta@rudrainnovative.in>
2023-07-19 12:40:35 +0530

    folder structure done

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { LoadingComponent } from './components/partials/loading/loading.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoadingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
